d4955368823383f29aee8234abe1ced3f314da17
MarcelArt <badman.islemon@gmail.com>
2026-06-20 22:50:27 +0700

    style - loading page not dark themed

--- shared/src/commonMain/kotlin/art/bangmarcel/passwordlesskotlin/App.kt
+++ shared/src/commonMain/kotlin/art/bangmarcel/passwordlesskotlin/App.kt
@@ -2,8 +2,10 @@ package art.bangmarcel.passwordlesskotlin
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -34,11 +36,13 @@ fun App(secureTokenManager: SecureTokenManager) {
     ) {
         when (authState) {
             AuthState.LOADING -> {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    CircularProgressIndicator()
+                Scaffold { paddingValues ->
+                    Box(
+                        modifier = Modifier.fillMaxSize().padding(paddingValues),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        CircularProgressIndicator()
+                    }
                 }
             }
             AuthState.UNAUTHENTICATED -> Navigator(LoginScreen())
